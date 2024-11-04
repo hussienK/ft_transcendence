e1484b710df1e2f36a561f360ea8b28b0205a31f
hussienK <hussienkenaan93@gmail.com>
2024-11-04 09:56:54 +0200

    move into Rest APIs

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TestModel
-from accounts.models import TranscendenceUser
-
-# Register your models here.
-admin.site.register(TestModel)
-admin.site.register(TranscendenceUser)
